90e639971b692e5bd48d80910576b09f5e012091
Sarah Ahmad <sarahahmad2019@gmail.com>
2019-10-02 21:02:14 -0500

    set up firebase and public website

---
+++ new-shopping-cart/build/precache-manifest.6617a7fde6332b5ff6168ceec9c24377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f7e1af1a1a0c56590547a1b090b2ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48fc721f674d9a66d2c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e5f8fa037ae8081415e6",
+    "url": "/static/js/2.89cd1617.chunk.js"
+  },
+  {
+    "revision": "b48fc721f674d9a66d2c",
+    "url": "/static/js/main.e0616075.chunk.js"
+  },
+  {
+    "revision": "cfd85b4678887566b37e",
+    "url": "/static/js/runtime-main.a58aba83.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
